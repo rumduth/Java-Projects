371e98ecd98338a4cbde19dcc64b088fd39037b6
Duth Ng <rumduth1@gmail.com>
2024-06-02 09:10:21 -0500

    Added Package

---
+++ Object Oriented Programming/Package/Demo2.java
@@ -0,0 +1,10 @@
+package mypack1;
+
+public class Demo2 {
+
+    Demo1 d = new Demo1();
+
+    public void display() {
+        System.out.println(d.a + d.b + d.c + d.d);
+    }
+}
---
+++ Object Oriented Programming/Package/Test2.java
@@ -0,0 +1,9 @@
+import mypack1.Demo1;
+
+public class Test2 {
+    public static void main(String args[]) {
+        Demo1 d1 = new Demo1();
+        d1.display();
+        System.out.println(d1.a + d1.b + d1.c + d1.d);
+    }
+}
